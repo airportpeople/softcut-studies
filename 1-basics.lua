d4b150652b8b6727794139fbc91f5535f8a6a869
Leon <airportpeoplemusic@gmail.com>
2024-10-26 13:14:53 -0400

    add window length, change sample

--- 1-basics.lua
+++ 1-basics.lua
@@ -2,13 +2,16 @@
 --
 -- E1 rate
 -- E2 loop start
--- E3 loop end
+-- E3 loop length
+-- K2 start
+-- K3 stop
 
  -- note: audio files must be 48kHz for accurate playback and looping
-file = _path.dust.."code/softcut-studies/lib/whirl1.aif"
+file = _path.dust.."audio/ap/f9m/1_pad_1.wav"
 rate = 1.0
 loop_start = 1.0
-loop_end = 2.0
+loop_len = 1.0
+loop_end = loop_start + loop_len
 
 function init()
   -- print file information (see function at the bottom)
@@ -42,31 +45,48 @@ end
 
 function enc(n,d)
   if n==1 then
-    rate = util.clamp(rate+d/100,-4,4)
+    rate = util.clamp(rate+d/2,-4,4)
     softcut.rate(1,rate)
   elseif n==2 then
-    loop_start = util.clamp(loop_start+d/100,1,loop_end)
+    loop_start = util.clamp(loop_start+d/10,1,duration)
     softcut.loop_start(1,loop_start)
   elseif n==3 then
-    loop_end = util.clamp(loop_end+d/100,loop_start,2)
-    softcut.loop_end(1,loop_end)
+    loop_len = util.clamp(loop_len+d/10,1,duration - loop_start)
   end
+
+  loop_end = loop_start + loop_len
+  softcut.loop_end(1,loop_end)
+
   redraw()
+end
+
+function key(n,z)
+  -- start
+  if n == 2 and z == 1 then
+    softcut.play(1, 1)
+  -- stop
+  elseif n == 3 and z == 1 then
+    softcut.play(1, 0)
+  end
 end
 
 function redraw()
   screen.clear()
-  screen.move(10,30)
+  screen.move(10,20)
   screen.text("rate: ")
-  screen.move(118,30)
+  screen.move(118,20)
   screen.text_right(string.format("%.2f",rate))
-  screen.move(10,40)
+  screen.move(10,30)
   screen.text("loop_start: ")
-  screen.move(118,40)
+  screen.move(118,30)
   screen.text_right(string.format("%.2f",loop_start))
-  screen.move(10,50)
+  screen.move(10,40)
+  screen.text("loop_len: ")
+  screen.move(118,40)
+  screen.text_right(string.format("%.2f",loop_len))
+  screen.move(10,60)
   screen.text("loop_end: ")
-  screen.move(118,50)
+  screen.move(118,60)
   screen.text_right(string.format("%.2f",loop_end))
   screen.update()
 end
@@ -75,7 +95,7 @@ end
 function print_info(file)
   if util.file_exists(file) == true then
     local ch, samples, samplerate = audio.file_info(file)
-    local duration = samples/samplerate
+    duration = samples/samplerate
     print("loading file: "..file)
     print("  channels:\t"..ch)
     print("  samples:\t"..samples)
